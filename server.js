f88e9b5f1f3dc7028b3ef195333e6ddf742bc194
Shakshi-kumari <pandeyshakshi000564@gmail.com>
2024-06-14 01:35:39 +0530

    inital commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const tasksRouter = require('./routes/tasks');
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+// Use the tasks router for the /tasks endpoint
+app.use('/tasks', tasksRouter);
+
+// Basic route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
